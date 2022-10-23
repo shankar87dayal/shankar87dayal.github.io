d9128237a1cc850a306c02fc67442c0cb9135df3
shankar87dayal <shankar.87.dayal@gmail.com>
2022-10-23 22:58:20 +0530

    add base url in user service file

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL='http://localhost:8181'
+// const BASE_URL='http://localhost:8181'
+export const BASE_URL='https://ecommerce-back-apis.herokuapp.com/'
 
 export const createUser=(data)=>{
     return axios.post(`${BASE_URL}/users/`,data).then(response=>response.data)
